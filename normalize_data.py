83604ecaf28c5852f123d020d49d78ec61a54dd2
Samuel_Santos <samucas878@gmail.com>
2025-11-10 10:56:43 -0300

    Baixando as dependências, e ajustando a leitura de extensão

--- normalize_data.py
+++ normalize_data.py
@@ -8,13 +8,18 @@ os.makedirs(output_dir, exist_ok=True)
 
 for filename in os.listdir(input_dir):
     input_file = os.path.join(input_dir, filename)
-    output_file = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}.parquet")
+    file, ext = os.path.splitext(filename)
+    output_file = os.path.join(output_dir, f"{file}.parquet")
     
-    if filename.endswith('.csv'):
+    if ext.lower() == '.csv':
         df = pd.read_csv(input_file)
-    elif filename.endswith('.json'):
-        df = pd.read_json(input_file, lines=True)
+    elif ext.lower() == '.json':
+        try:
+            df = pd.read_json(input_file)
+        except ValueError:
+            df = pd.read_json(input_file, lines=True)
     else:
+        print(f"Formato de arquivo não suportado: {filename}!")
         continue  # pular arquivos que não são csv ou json
     
     # Normalização: converter nomes de colunas para minúsculas e substituir espaços por underscores
