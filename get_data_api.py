bf0c4976d840bab6996d0a97a4d2eea61a0f8b31
Samuel_Santos <samucas878@gmail.com>
2025-11-10 00:11:50 -0300

    Criando uma lista vazia, adicionando ceps à lista, e transformando ela em um DataFrame

--- get_data_api.py
+++ get_data_api.py
@@ -1,8 +1,6 @@
 import requests
 import pandas as pd
 
-user_list = []
-
 def get_user(cep):
     try:
         endpoint = f"https://viacep.com.br/ws/{cep}/json/"
@@ -18,7 +16,12 @@ users_path = "bronze_raw/users.csv"
 users = pd.read_csv(users_path)
 
 cep_list = users["cep"].tolist()
+users_list = []
 
 for cep in cep_list:
     new_cep = get_user(cep.replace("-", ""))
+    users_list.append(new_cep)
     print(new_cep)
+
+users_df = pd.DataFrame(users_list)
+users_df.to_csv("bronze_raw/users_info.csv", index=False)
