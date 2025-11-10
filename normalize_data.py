dfd69f31417bb1967b870b6f52eb96340026cead
Samuel_Santos <samucas878@gmail.com>
2025-11-10 11:23:33 -0300

    - Transformando a chamada em uma classe
    - Removendo os arquivos para fins de teste da classe recém criada

--- normalize_data.py
+++ normalize_data.py
@@ -1,28 +1,33 @@
 import os
 import pandas as pd
 
-input_dir = '1_bronze_raw'  # vamos ler os arquivos da pasta bronze (csv e json)
-output_dir = '2_silver_validated'  # vamos salvar como arquivo .parquet
+class NormalizedData:
+    def __init__(self, input_dir, output_dir):
+        self.input_dir = input_dir
+        self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
 
-os.makedirs(output_dir, exist_ok=True)
+    def normalize(self):
+        for file in os.listdir(self.input_dir):
+            input_file = os.path.join(self.input_dir, file)
+            output_file = os.path.join(self.output_dir, f"{file}.parquet")
+            file, ext = os.path.splitext(file)
+            
+            if ext.lower() == '.csv':
+                df = pd.read_csv(input_file)
+            elif ext.lower() == '.json':
+                try:
+                    df = pd.read_json(input_file)
+                except ValueError:
+                    df = pd.read_json(input_file, lines=True)
+            else:
+                print(f"Formato de arquivo não suportado: {file}!")
+                continue
+            
+            df.columns = [col.lower().replace(' ', '_') for col in df.columns]
+            
+            df.to_parquet(output_file, index=False)
 
-for filename in os.listdir(input_dir):
-    input_file = os.path.join(input_dir, filename)
-    file, ext = os.path.splitext(filename)
-    output_file = os.path.join(output_dir, f"{file}.parquet")
-    
-    if ext.lower() == '.csv':
-        df = pd.read_csv(input_file)
-    elif ext.lower() == '.json':
-        try:
-            df = pd.read_json(input_file)
-        except ValueError:
-            df = pd.read_json(input_file, lines=True)
-    else:
-        print(f"Formato de arquivo não suportado: {filename}!")
-        continue  # pular arquivos que não são csv ou json
-    
-    # Normalização: converter nomes de colunas para minúsculas e substituir espaços por underscores
-    df.columns = [col.lower().replace(' ', '_') for col in df.columns]
-    
-    df.to_parquet(output_file, index=False)
+if __name__ == "__main__":
+    normalizer = NormalizedData(input_dir='1_bronze_raw', output_dir='2_silver_validated')
+    normalizer.normalize()
