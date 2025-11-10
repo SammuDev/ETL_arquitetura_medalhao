c12662447f01c9b0531e6951367ebe11093c5f02
Samuel_Santos <samucas878@gmail.com>
2025-11-09 23:23:38 -0300

    Criando arquivos de usuários e produtos

--- get_data_api.py
+++ get_data_api.py
@@ -1,15 +1,13 @@
 import requests
+import pandas as pd
 
-def get_cep(cep):
+user_list = []
+
+def get_user(cep):
     endpoint = f"https://viacep.com.br/ws/{cep}/json/"
     response = requests.get(endpoint)
-    cep_info = response.json()
+    user_info = response.json()
 
-    return cep_info
+    return user_info
 
-cep_info = get_cep("49043349")
 
-cep_list = ["49043349", "01001000", "20040002"]
-
-for i in cep_list:
-    print(get_cep(i))
