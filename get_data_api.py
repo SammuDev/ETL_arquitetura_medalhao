2be3064f01ad916a214887a6c4ebec9503ab79b3
Samuel_Santos <samucas878@gmail.com>
2025-11-09 22:48:17 -0300

    Corrigindo endereço da API

--- get_data_api.py
+++ get_data_api.py
@@ -1,8 +1,15 @@
 import requests
 
 def get_cep(cep):
-    endpoint = f"viacep.com.br/ws/{cep}/json/"
+    endpoint = f"https://viacep.com.br/ws/{cep}/json/"
     response = requests.get(endpoint)
     cep_info = response.json()
 
-get_cep("01001000")
+    return cep_info
+
+cep_info = get_cep("49043349")
+
+cep_list = ["49043349", "01001000", "20040002"]
+
+for i in cep_list:
+    print(get_cep(i))
