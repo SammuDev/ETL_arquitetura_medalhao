6c8be0d21c897461cb5edc5d669d5125fd4ef06e
Samuel_Santos <samucas878@gmail.com>
2025-11-09 23:56:02 -0300

    Aplicando tratamento de erro

--- get_data_api.py
+++ get_data_api.py
@@ -4,20 +4,21 @@ import pandas as pd
 user_list = []
 
 def get_user(cep):
-    endpoint = f"https://viacep.com.br/ws/{cep}/json/"
-    response = requests.get(endpoint)
-    user_info = response.json()
+    try:
+        endpoint = f"https://viacep.com.br/ws/{cep}/json/"
+        response = requests.get(endpoint)
+        user_info = response.json()
 
-    return user_info
+        return user_info
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 users_path = "bronze_raw/users.csv"
 users = pd.read_csv(users_path)
 
 cep_list = users["cep"].tolist()
 
-print(cep_list)
-
-"""
-_users = pd.DataFrame(users)
-print(df_users.head())
-"""
+for cep in cep_list:
+    new_cep = get_user(cep.replace("-", ""))
+    print(new_cep)
