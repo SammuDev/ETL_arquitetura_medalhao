d0067a30cde1f0a7da29bc91a2afb5b56cd90072
Samuel_Santos <samucas878@gmail.com>
2025-11-10 11:50:59 -0300

    Organizando a chamada de função

--- normalize_data.py
+++ normalize_data.py
@@ -7,26 +7,28 @@ class NormalizedData:
         self.output_dir = output_dir
         os.makedirs(self.output_dir, exist_ok=True)
 
+    def load_data(self, file, ext):
+        input_file = os.path.join(self.input_dir, file)
+            
+        if ext.lower() == '.csv':
+                df = pd.read_csv(input_file)
+        elif ext.lower() == '.json':
+            try:
+                df = pd.read_json(input_file)
+            except ValueError:
+                df = pd.read_json(input_file, lines=True)
+
+        return df
+
     def normalize(self):
         for file in os.listdir(self.input_dir):
-            input_file = os.path.join(self.input_dir, file)
-            output_file = os.path.join(self.output_dir, f"{file}.parquet")
-            file, ext = os.path.splitext(file)
+            name, ext = os.path.splitext(file)
+            output_file = os.path.join(self.output_dir, f"{name}.parquet")
             
-            if ext.lower() == '.csv':
-                df = pd.read_csv(input_file)
-            elif ext.lower() == '.json':
-                try:
-                    df = pd.read_json(input_file)
-                except ValueError:
-                    df = pd.read_json(input_file, lines=True)
-            else:
-                print(f"Formato de arquivo não suportado: {file}!")
-                continue
-            
-            df.columns = [col.lower().replace(' ', '_') for col in df.columns]
+            df = self.load_data(file, ext)
             
             df.to_parquet(output_file, index=False)
+            print(f"Normalized file saved to: {output_file}")
 
 if __name__ == "__main__":
     normalizer = NormalizedData(input_dir='1_bronze_raw', output_dir='2_silver_validated')
