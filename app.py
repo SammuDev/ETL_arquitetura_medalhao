d3c8500db495a35ed170d38550edf28b11443a38
Samuel_Santos <samucas878@gmail.com>
2025-11-12 01:10:12 -0300

    Visualizacao com SQL as tabelas no banco de dados

--- app.py
+++ app.py
@@ -6,14 +6,9 @@ db = DB(host="localhost", port=5432, database="postgres", user="postgres", passw
 
 for file in os.listdir("2_silver_validated/"):
     name, ext = os.path.splitext(file)
-    #if ext == ".parquet":
-    #    table_name = name
-    #    df = pd.read_parquet(f"2_silver_validated/{file}")
+    if ext == ".parquet":
+        table_name = name
+        df = pd.read_parquet(f"2_silver_validated/{file}")
         
-    #    db.create_table(table_name, df)
-    #    db.insert_data(table_name, df)
-
-    df = pd.read_parquet(f"2_silver_validated/{file}")
-
-    db.create_table(file.replace(".parquet", ""), df)
-    db.insert_data(file.replace(".parquet", ""), df)
+        db.create_table(table_name, df)
+        db.insert_data(table_name, df)
